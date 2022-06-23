6c90ccaca801a5d8754f07b885fd21c9d8ad4d58
Daniel-R23 <danielima1023@gmail.com>
2022-06-23 11:35:33 -0300

    método de adicionar veículo ao pátio implementado

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
 (function () {
     var _a;
     const $ = (query) => document.querySelector(query);
+    function patio() {
+        function ler() {
+        }
+        function adicionar(veiculo) {
+            var _a;
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${veiculo.nome}</td>
+                <td>${veiculo.placa}</td>
+                <td>${veiculo.entrada}</td>
+                <td><button class="delete" data-placa="${veiculo.placa}">X</button></td>
+            `;
+            (_a = $('#patio')) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+        }
+        function remover() {
+        }
+        function salvar() {
+        }
+        function renderizar() {
+        }
+        return { ler, adicionar, remover, salvar, renderizar };
+    }
     (_a = $('#cadastrar')) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b;
         const nome = (_a = $('#nome')) === null || _a === void 0 ? void 0 : _a.value;
@@ -10,5 +32,6 @@
             alert('Os campos nome e placa são obrigatórios');
             return;
         }
+        patio().adicionar({ nome, placa, entrada: new Date() });
     });
 })();
