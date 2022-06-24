a2437b97bb765d029fe9555341425f3f97234f44
Daniel-R23 <danielima1023@gmail.com>
2022-06-24 12:08:37 -0300

    função de remoção implementada

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-"use strict";
 (function () {
     var _a;
     const $ = (query) => document.querySelector(query);
+    function calcTempo(mil) {
+        const min = Math.floor(mil / 60000);
+        const seg = Math.floor((mil % 60000) / 1000);
+        return `${min} minutos e ${seg} segundos`;
+    }
     function patio() {
         function ler() {
             return localStorage.patio ? JSON.parse(localStorage.patio) : [];
         }
         function adicionar(veiculo, salva) {
-            var _a;
+            var _a, _b;
             const row = document.createElement('tr');
             row.innerHTML = `
                 <td>${veiculo.nome}</td>
@@ -15,11 +19,20 @@
                 <td>${veiculo.entrada}</td>
                 <td><button class="delete" data-placa="${veiculo.placa}">X</button></td>
             `;
-            (_a = $('#patio')) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+            (_a = row.querySelector(".delete")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+                remover(this.dataset.placa);
+            });
+            (_b = $('#patio')) === null || _b === void 0 ? void 0 : _b.appendChild(row);
             if (salva)
                 salvar([...ler(), veiculo]);
         }
-        function remover() {
+        function remover(placa) {
+            const { entrada, nome } = ler().find(veiculo => veiculo.placa === placa);
+            const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime());
+            if (!confirm(`O veículo ${nome} permaneceu por ${tempo}. Deseja encerrar?`))
+                return;
+            salvar(ler().filter(veiculo => veiculo.placa !== placa));
+            renderizar();
         }
         function salvar(veiculos) {
             localStorage.setItem("patio", JSON.stringify(veiculos));
@@ -42,6 +55,6 @@
             alert('Os campos nome e placa são obrigatórios');
             return;
         }
-        patio().adicionar({ nome, placa, entrada: new Date() }, true);
+        patio().adicionar({ nome, placa, entrada: new Date().toISOString() }, true);
     });
 })();
--- script.ts
+++ script.ts
@@ -1,11 +1,18 @@
 interface Veiculo{
     nome: string;
     placa: string;
-    entrada: Date;
+    entrada: Date | string;
 }
 
 (function(){
     const $ = (query:string): HTMLInputElement | null => document.querySelector(query);
+
+    function calcTempo(mil:number){
+        const min = Math.floor(mil / 60000);
+        const seg = Math.floor((mil % 60000) / 1000);
+
+        return `${min} minutos e ${seg} segundos`;
+    }
 
     function patio(){
         function ler(): Veiculo[]{
@@ -19,12 +26,22 @@ interface Veiculo{
                 <td>${veiculo.entrada}</td>
                 <td><button class="delete" data-placa="${veiculo.placa}">X</button></td>
             `;
+
+            row.querySelector(".delete")?.addEventListener("click", function(){
+                remover(this.dataset.placa);
+            });
+
             $('#patio')?.appendChild(row);
 
             if(salva) salvar([...ler(), veiculo])
         }
-        function remover(){
+        function remover(placa: string){
+            const {entrada, nome} = ler().find(veiculo => veiculo.placa === placa);
+            const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime());
 
+            if(!confirm(`O veículo ${nome} permaneceu por ${tempo}. Deseja encerrar?`)) return;
+            salvar(ler().filter(veiculo => veiculo.placa !== placa));
+            renderizar();
         }
         function salvar(veiculos: Veiculo[]){
             localStorage.setItem("patio",  JSON.stringify(veiculos));
@@ -50,6 +67,6 @@ interface Veiculo{
             return;
         }
 
-        patio().adicionar({nome, placa, entrada: new Date()}, true);
+        patio().adicionar({nome, placa, entrada: new Date().toISOString()}, true);
     })
 })()
